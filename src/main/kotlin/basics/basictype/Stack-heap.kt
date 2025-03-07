bc3ca7481ecd95cb6fb64a7f82952f8148a0d3be
talipbozkurt <talipbozkurt19@gmail.com>
2025-03-07 17:54:35 +0300

    Stack and heap comparison example

---
+++ src/main/kotlin/basics/basictype/Stack-heap.kt
@@ -0,0 +1,23 @@
+package org.example.basics.basictype
+
+fun main() {
+
+   /* "The `Int` variable type is stored on the stack and is static. "
+    It points to the value of the variable.
+    Arrays, on the other hand, are stored on the heap and point to the reference of the variable."*/
+
+
+    var sayi1 = 10
+    var sayi2: Int
+    sayi2 = sayi1
+    sayi1 = 20
+    println(" sayi1 = ${sayi1}  sayi2 = ${sayi2}") // sayi1 = 20 sayi2 = 10
+
+
+    var nullableSayi1: Array<Int?> = arrayOf(10)
+    var nullableSayi2: Array<Int?> = nullableSayi1
+
+    nullableSayi1[0] = 20
+
+    println("nullableSayi1 = ${nullableSayi1[0]}  nullableSayi2 = ${nullableSayi2[0]}") //nullableSayi1 = 20  nullableSayi2 = 20
+}
